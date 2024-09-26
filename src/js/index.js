037cb2cf4999d26f87182c23b356f99d777f444b
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-09-26 19:30:41 -0300

    spa code in js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
+import { Router } from "./router.js"
 
+const router = new Router()
+
+router.add("/", "/src/pages/home.html")
+router.add("/universe", "/src/pages/universe.html")
+router.add("/explorer", "/src/pages/explorer.html")
+router.add(404, "/src/pages/404.html")
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
